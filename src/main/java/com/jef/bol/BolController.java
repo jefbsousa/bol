84da34c3548261dbf60d895128f36277ba41312c
jefbsousa <jef.b.sousa@gmail.com>
2022-05-02 22:23:41 -0300

    dirty files, working by saving pdf and retrieving to byte to send to the API

--- src/main/java/com/jef/bol/BolController.java
+++ src/main/java/com/jef/bol/BolController.java
@@ -1,13 +1,18 @@
 package com.jef.bol;
 
+import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
+import javax.xml.transform.TransformerException;
+import java.io.*;
 
 @RestController
 @RequestMapping("/boleto")
@@ -17,8 +22,26 @@ public class BolController {
     private BolService bolService;
 
     @GetMapping("/generate")
-    public ResponseEntity<String> getPdf() throws IOException {
-        bolService.generateHtmlToPdf();
-        return new ResponseEntity<>("OK" ,HttpStatus.OK);
+    public ResponseEntity<InputStreamResource> getPdf() throws IOException, TransformerException {
+//        bolService.generateHtmlToPdf();
+        byte[] pdfBytes = bolService.generateHtmlToPdf();
+        InputStream targetStream = new ByteArrayInputStream(pdfBytes);
+
+//        File file = new File(fileName);
+//        FileUtils.writeByteArrayToFile(file, pdfBytes); //org.apache.commons.io.FileUtils
+        InputStreamResource resource = new InputStreamResource(targetStream);
+        MediaType mediaType = MediaType.parseMediaType("application/pdf");
+
+        return ResponseEntity.ok()
+                // Content-Disposition
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=" + "XXX.pdf")
+                // Content-Type
+                .contentType(mediaType)
+                // Contet-Length
+//                .contentLength(file.length()) //length
+                .body(resource);
+
+//        return new ResponseEntity<>( ,HttpStatus.OK);
+
     }
 }
--- src/main/java/com/jef/bol/BolService.java
+++ src/main/java/com/jef/bol/BolService.java
@@ -1,17 +1,22 @@
 package com.jef.bol;
 
+import com.lowagie.text.pdf.PdfWriter;
 import com.openhtmltopdf.pdfboxout.PdfRendererBuilder;
 import org.jsoup.Jsoup;
 import org.jsoup.helper.W3CDom;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.springframework.stereotype.Service;
+import org.xhtmlrenderer.layout.SharedContext;
+import org.xhtmlrenderer.pdf.ITextRenderer;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
+import javax.xml.transform.TransformerException;
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 
 @Service
@@ -20,10 +25,10 @@ public class BolService {
     private static final String HTML = "src/main/resources/template.html";
     private static final String PDF = "src/main/resources/test.pdf";
 
-    public void generateHtmlToPdf() throws IOException {
+    public byte[] generateHtmlToPdf() throws IOException, TransformerException {
         File inputHTML = new File(HTML);
         Document doc = createWellFormedHtml(inputHTML);
-        xhtmlToPdf(doc, PDF);
+        return xhtmlToPdf(doc, PDF);
     }
 
     private Document createWellFormedHtml(File inputHTML) throws IOException {
@@ -31,7 +36,7 @@ public class BolService {
         Element link = doc.select("p#interestRate").first();
         System.out.println(link.text());
         link.appendText("JJ");
-        link.text("XXX");
+        link.text("1234.55");
 
         doc.select("p#interestRate").attr("text", "11");
         doc.outputSettings()
@@ -39,17 +44,72 @@ public class BolService {
         return doc;
     }
 
-    private void xhtmlToPdf(Document doc, String outputPdf) throws IOException {
-        try (OutputStream os = new FileOutputStream(outputPdf)) {
-            String baseUri = FileSystems.getDefault()
-                    .getPath("src/main/resources/")
-                    .toUri()
-                    .toString();
-            PdfRendererBuilder builder = new PdfRendererBuilder();
-            builder.withUri(outputPdf);
-            builder.toStream(os);
-            builder.withW3cDocument(new W3CDom().fromJsoup(doc), baseUri);
-            builder.run();
-        }
+    private byte[] xhtmlToPdf(Document doc, String outputPdf) throws IOException, TransformerException {
+        OutputStream os = new FileOutputStream(outputPdf);
+
+        String baseUri = FileSystems.getDefault()
+                .getPath("src/main/resources/")
+                .toUri()
+                .toString();
+        PdfRendererBuilder builder = new PdfRendererBuilder();
+        builder.withUri(outputPdf);
+        builder.toStream(os);
+        builder.withW3cDocument(new W3CDom().fromJsoup(doc), baseUri);
+        builder.run();
+
+
+        byte[] array = Files.readAllBytes(Paths.get("src/main/resources/test.pdf"));
+        return array;
+
+//        return InputStream();
+
+//        com.lowagie.text.Document document = new com.lowagie.text.Document();
+//        ByteArrayOutputStream out = new ByteArrayOutputStream();
+//        PdfWriter writer = PdfWriter.getInstance(document, out);
+//
+//        byte[] pdf = out.toByteArray();
+//        return pdf;
+
+//        String str = doc.toString();
+//        byte[] b;
+//        try {
+//            b = str.getBytes("UTF-8");
+//        } catch (UnsupportedEncodingException e) {
+//            // TODO Auto-generated catch block
+//            e.printStackTrace();
+//        }
+//        b = str.getBytes(Charset.forName("UTF-8"));
+//        b = str.getBytes(StandardCharsets.UTF_8);
+//        return b;
+
+        /***************************/
+//        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+//        Transformer transformer = transformerFactory.newTransformer();
+//
+//        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+//        StreamResult result = new StreamResult(bos);
+//
+//        transformer.transform((Source) doc, result);
+//        byte []array = bos.toByteArray();
+//        InputStream targetStream = new ByteArrayInputStream(array);
+//        return  targetStream;
+
+
+        /***************************/
+
+
+//        Document document = new Document();
+//
+//        ByteArrayOutputStream out = new ByteArrayOutputStream();
+//
+//        PdfWriter writer = PdfWriter.getInstance(document, out);
+//        document.open();
+//
+//        InputStream in = IOUtils.toInputStream(htmlString);
+//        XMLWorkerHelper.getInstance().parseXHtml(writer, document, in);
+//        document.close();
+//
+//        return out.toByteArray();
+
     }
 }
